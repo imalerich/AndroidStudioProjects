293886c1664b592337c75c2d67107019540e8b54
Malerich <wdmalerich@iastate.edu>
2015-12-03 21:33:56 -0600

    All the image addresses are loaded and can individually be set to an ImageView

--- Photostream/app/src/main/java/com/ian/malerich/photostream/PhotostreamActivity.java
+++ Photostream/app/src/main/java/com/ian/malerich/photostream/PhotostreamActivity.java
@@ -1,22 +1,91 @@
 package com.ian.malerich.photostream;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class PhotostreamActivity extends AppCompatActivity {
+    private RequestQueue queue;
+    private ArrayList<String> images = new ArrayList<>();
+    private int currentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        queue = Volley.newRequestQueue(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photostream);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // create a Volley request for the text view
+        StringRequest strreq = new StringRequest(Request.Method.GET, "http://photostream.iastate.edu/api/photo?key=c88ba8aca62dd5ccb60d", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray arr = new JSONArray(response);
+
+                    for (int i = 0; i < arr.length(); i++) {
+                        JSONObject obj = arr.getJSONObject(i);
+                        images.add(obj.getString("image_medium"));
+                    }
+
+                    setImageViewWithURL(images.get(currentIndex));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //
+            }
+        });
+
+        queue.add(strreq);
+    }
+
+    public void userDidTapImage(View view) {
+        currentIndex = (currentIndex + 1) % images.size();
+        setImageViewWithURL(images.get(currentIndex));
+    }
+
+    private void setImageViewWithURL(String url) {
+        // load the image for the image view
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        ImageRequest imgreq = new ImageRequest(url,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap bitmap) {
+                        imageView.setImageBitmap(bitmap);
+                    }
+                }, 0, 0, null,
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //
+                    }
+                });
+
+        queue.add(imgreq);
     }
 
     @Override
